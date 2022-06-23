97433f83f1595e7aba300fbbad5181ca5a321ccb
Stijn Van Campenhout <stijn.vancampenhout@continu-it.be>
2022-06-23 15:36:11 +0200

    implement fyne

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/subutux/hass_companion
 go 1.18
 
 require (
+	fyne.io/fyne/v2 v2.2.1
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/k0kubun/pp/v3 v3.1.0
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
@@ -10,11 +11,34 @@ require (
 )
 
 require (
+	fyne.io/systray v1.10.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
+	github.com/fyne-io/glfw-js v0.0.0-20220120001248-ee7290d23504 // indirect
+	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20211213063430-748e38ca8aec // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sacOO7/go-logger v0.0.0-20180719173527-9ac9add5a50d // indirect
+	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564 // indirect
+	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9 // indirect
+	github.com/stretchr/testify v1.7.2 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
+	github.com/yuin/goldmark v1.4.0 // indirect
+	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
+	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
 	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 )
--- main.go
+++ main.go
@@ -2,51 +2,67 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
 	"time"
 
-	"github.com/k0kubun/pp/v3"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/widget"
 	"github.com/subutux/hass_companion/internal/auth"
 	"github.com/subutux/hass_companion/internal/hass"
 	"github.com/subutux/hass_companion/internal/ws"
 )
 
 var connected bool
+var homeAssistant *hass.Hass
 
 func main() {
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
+	a := app.New()
+	w := a.NewWindow("Status")
 
-	connected = false
-	creds, err := auth.Initiate("https://home.assistant.subutux.be")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = creds.Authorize()
-	if err != nil {
-		log.Fatal(err)
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu("MyApp",
+			fyne.NewMenuItem("Status", func() {
+				w.Show()
+			}))
+		desk.SetSystemTrayMenu(m)
 	}
 
-	pp.Println(creds)
+	statusStringLabel := widget.NewLabel("loading")
 
-	homeAssistant := hass.NewHass("wss://home.assistant.subutux.be", creds)
-	homeAssistant.Connect()
-	for !homeAssistant.Connected {
-		time.Sleep(time.Second * 1)
-		log.Print("waiting for connection...")
-	}
-	homeAssistant.SubscribeToEventType("state_changed", func(message []byte, conn *ws.Websocket) {
-		log.Printf("Received event: %s", string(message))
+	w.SetContent(statusStringLabel)
+	w.SetCloseIntercept(func() {
+		w.Hide()
 	})
 
-	for {
-		select {
-		case <-interrupt:
-			log.Println("interrupt")
-			homeAssistant.Close()
-			return
+	go func() {
+		connected = false
+		statusStringLabel.SetText("Retrieving authentication ...")
+		creds, err := auth.Initiate("https://home.assistant.subutux.be")
+		if err != nil {
+			log.Fatal(err)
 		}
-	}
+		err = creds.Authorize()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		statusStringLabel.SetText("Connecting ...")
+
+		homeAssistant = hass.NewHass("wss://home.assistant.subutux.be", creds)
+		homeAssistant.Connect()
+		for !homeAssistant.Connected {
+			time.Sleep(time.Second * 1)
+			log.Print("waiting for connection...")
+			statusStringLabel.SetText("waiting for connection...")
+		}
+		statusStringLabel.SetText("Connected")
+		homeAssistant.SubscribeToEventType("state_changed", func(message []byte, conn *ws.Websocket) {
+			log.Printf("Received event: %s", string(message))
+		})
+	}()
+
+	w.ShowAndRun()
+	homeAssistant.Close()
 }
