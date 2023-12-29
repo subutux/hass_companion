ad30db549cfcfe8161ba45f7d593a6a40b66711c
Stijn Van Campenhout <stijn.vancampenhout@continu-it.be>
2023-12-29 13:59:08 +0100

    fix: add source to log output

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -9,9 +9,16 @@ import (
 var logger *slog.Logger
 var once sync.Once
 
+// I gives back an instance of the slog logger
 func I() *slog.Logger {
 	once.Do(func() {
-		logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+		logger = slog.New(slog.NewTextHandler(
+			os.Stdout,
+			&slog.HandlerOptions{
+				AddSource: true,
+			}),
+		)
 	})
+
 	return logger
 }
